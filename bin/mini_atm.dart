7828b93bb5f0d50e0d9e951da04b82a28e1966e1
Anandhu Udayan <anandhuudayan180@gmail.com>
2025-10-28 16:28:48 +0530

    feat: add continuous menu loop and balance variable setup

--- bin/mini_atm.dart
+++ bin/mini_atm.dart
@@ -1,30 +1,33 @@
 import 'dart:io';
 
 void main(List<String> args) {
-  print(
-    "Welcome to Mini ATM \nPlease select an option:\n1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit",
-  );
+  double balance = 0.0;
+  while (true) {
+    print(
+      "Welcome to Mini ATM \nPlease select an option:\n1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit",
+    );
 
-  String? userSelectedMenu = stdin.readLineSync();
-  if (userSelectedMenu == null) {
-    print("No input provided");
-    return;
-  }
-  int number = int.parse(userSelectedMenu);
-  switch (number) {
-    case 1:
-      print("You selected Deposit option");
-      break;
-    case 2:
-      print("You selected Withdraw option");
-      break;
-    case 3:
-      print("You selected check Balance option");
-      break;
-    case 4:
-      print("You selected Exit option");
-      break;
-    default:
-      print("Invalid option selected");
+    String? userSelectedMenu = stdin.readLineSync();
+    if (userSelectedMenu == null) {
+      print("No input provided");
+      return;
+    }
+    int number = int.parse(userSelectedMenu);
+    switch (number) {
+      case 1:
+        print("You selected Deposit option");
+        break;
+      case 2:
+        print("You selected Withdraw option");
+        break;
+      case 3:
+        print("You selected check Balance option");
+        break;
+      case 4:
+        print("You selected Exit option");
+        exit(0);
+      default:
+        print("Invalid option selected");
+    }
   }
 }
