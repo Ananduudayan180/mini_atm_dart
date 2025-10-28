35429e43daa65d58426ec541dc2714a3cab0cd60
Anandhu Udayan <anandhuudayan180@gmail.com>
2025-10-28 14:28:10 +0530

    add basic ATM menu and option selection

--- bin/mini_atm.dart
+++ bin/mini_atm.dart
@@ -1,5 +1,30 @@
-import 'package:mini_atm/mini_atm.dart' as mini_atm;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${mini_atm.calculate()}!');
+void main(List<String> args) {
+  print(
+    "Welcome to Mini ATM \nPlease select an option:\n1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit",
+  );
+
+  String? userSelectedMenu = stdin.readLineSync();
+  if (userSelectedMenu == null) {
+    print("No input provided");
+    return;
+  }
+  int number = int.parse(userSelectedMenu);
+  switch (number) {
+    case 1:
+      print("You selected Deposit option");
+      break;
+    case 2:
+      print("You selected Withdraw option");
+      break;
+    case 3:
+      print("You selected check Balance option");
+      break;
+    case 4:
+      print("You selected Exit option");
+      break;
+    default:
+      print("Invalid option selected");
+  }
 }
