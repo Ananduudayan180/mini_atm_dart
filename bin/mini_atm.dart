1f44d3dd3555198d735a5c425186f9eb90a809d4
Anandhu Udayan <anandhuudayan180@gmail.com>
2025-10-28 11:56:44 +0530

    Initial commit: Mini ATM dart

---
+++ bin/mini_atm.dart
@@ -0,0 +1,5 @@
+import 'package:mini_atm/mini_atm.dart' as mini_atm;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mini_atm.calculate()}!');
+}
---
+++ test/mini_atm_test.dart
@@ -0,0 +1,8 @@
+import 'package:mini_atm/mini_atm.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
