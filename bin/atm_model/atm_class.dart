c5c3325e2b3bf9e5251af8f6c2fa2f4c73912ea7
Anandhu Udayan <anandhuudayan180@gmail.com>
2025-10-29 15:24:57 +0530

    feat: add PIN authentication and transaction history to ATM and minor improvements

--- bin/atm_model/atm_class.dart
+++ bin/atm_model/atm_class.dart
@@ -2,16 +2,19 @@ import 'dart:io';
 
 class ATM {
   double _balance = 0.0;
+  int _pin = 1234;
+  final List<String> _transactionHistory = [];
 
   void balanceInquiry() {
-    print("Your current balance is: RS.${_balance.toStringAsFixed(2)}");
+    print("Your current balance is: ₹${_balance.toStringAsFixed(2)}");
   }
 
   void deposit() {
     print("Enter amount to deposit:");
     double amount = inputValidation();
     _balance += amount;
-    print('successfully deposited RS.${amount.toStringAsFixed(2)}');
+    _transactionHistory.add('Deposited: ₹$amount');
+    print('successfully deposited ₹${amount.toStringAsFixed(2)}');
   }
 
   void withdraw() {
@@ -21,7 +24,19 @@ class ATM {
       print("Insufficient balance");
     } else {
       _balance -= amount;
-      print('successfully withdrawn RS.${amount.toStringAsFixed(2)}');
+      _transactionHistory.add('Withdrawn: ₹$amount');
+      print('successfully withdrawn ₹${amount.toStringAsFixed(2)}');
+    }
+  }
+
+  void showTransactionHistory() {
+    if (_transactionHistory.isEmpty) {
+      print("No transactions yet.");
+    } else {
+      print("Transaction History:");
+      for (String transaction in _transactionHistory) {
+        print(transaction);
+      }
     }
   }
 
@@ -30,22 +45,38 @@ class ATM {
       String? stringAmount = stdin.readLineSync();
       if (stringAmount == null) {
         print("No input provided");
-        return 0.0;
+        continue;
       } else {
         try {
-          double ammount = double.parse(stringAmount);
-          if (ammount <= 0) {
+          double amount = double.parse(stringAmount);
+          if (amount <= 0) {
             print(
               "Amount cannot be zero or negative. Please enter a valid number.",
             );
             continue;
           }
-          return ammount;
+          return amount;
         } catch (e) {
           print("Invalid amount entered. Please enter a valid number.");
           continue;
         }
       }
+    }
+  }
+
+  bool verifyPin() {
+    print('Enter your ATM pin:');
+    String? pinInput = stdin.readLineSync();
+    if (pinInput == null) {
+      print("No input provided");
+      exit(0);
+    }
+    if (pinInput == _pin.toString()) {
+      print("PIN verified successfully.");
+      return true;
+    } else {
+      print("Incorrect PIN. Access denied.");
+      return false;
     }
   }
 }
--- bin/mini_atm.dart
+++ bin/mini_atm.dart
@@ -3,36 +3,54 @@ import 'atm_model/atm_class.dart';
 
 void main(List<String> args) {
   ATM atm = ATM();
-  print("Welcome to Mini ATM");
-  while (true) {
-    print(
-      "Please select an option:\n1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit",
-    );
+  void showMenu() {
+    int? number;
+    print("Welcome to Mini ATM");
+    while (true) {
+      print(
+        "Please select an option:\n1. Deposit\n2. Withdraw\n3. Check Balance\n4. Transaction History\n5. Exit",
+      );
 
-    String? userSelectedMenu = stdin.readLineSync();
-    if (userSelectedMenu == null) {
-      print("No input provided");
-      return;
+      String? userSelectedMenu = stdin.readLineSync();
+      if (userSelectedMenu == null) {
+        print("No input provided");
+        return;
+      }
+      try {
+        number = int.parse(userSelectedMenu);
+      } catch (e) {
+        print("Invalid number selected. please enter a valid number");
+        continue;
+      }
+      switch (number) {
+        case 1:
+          print("You selected Deposit option");
+          atm.deposit();
+          break;
+        case 2:
+          print("You selected Withdraw option");
+          atm.withdraw();
+          break;
+        case 3:
+          print("You selected check Balance option");
+          atm.balanceInquiry();
+          break;
+        case 4:
+          print("You selected Transaction History option");
+          atm.showTransactionHistory();
+          break;
+        case 5:
+          print("You selected Exit option");
+          exit(0);
+        default:
+          print("Invalid option selected");
+      }
     }
-    int number = int.parse(userSelectedMenu);
-    switch (number) {
-      case 1:
-        print("You selected Deposit option");
-        atm.deposit();
-        break;
-      case 2:
-        print("You selected Withdraw option");
-        atm.withdraw();
-        break;
-      case 3:
-        print("You selected check Balance option");
-        atm.balanceInquiry();
-        break;
-      case 4:
-        print("You selected Exit option");
-        exit(0);
-      default:
-        print("Invalid option selected");
-    }
+  }
+
+  if (atm.verifyPin()) {
+    showMenu();
+  } else {
+    exit(0);
   }
 }
